2b3c162727eb46609fc9fb7d57d7d7da46673b75
Christian Skrøvseth <christian.skrovseth@gmail.com>
2025-12-03 22:05:05 +0100

    Mock Umami if missing

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,13 @@ import '@app/index.css';
 import { App } from '@app/App';
 import { render } from 'solid-js/web';
 
+if (typeof umami === 'undefined') {
+  window.umami = {
+    track: () => undefined,
+    identify: async () => undefined,
+  };
+}
+
 const root = document.getElementById('root');
 
 if (root === null) {
