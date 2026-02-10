c999e1c6efb7e17cce318f32b102d81b5473e796
Christian Skrøvseth <christian.skrovseth@gmail.com>
2026-02-10 10:47:00 +0100

    Improve Umami mock

--- src/index.tsx
+++ src/index.tsx
@@ -3,9 +3,16 @@ import { App } from '@app/App';
 import { render } from 'solid-js/web';
 
 if (typeof umami === 'undefined') {
+  console.debug('Umami is not defined, setting up a mock.');
+
   window.umami = {
-    track: () => undefined,
-    identify: async () => undefined,
+    track: (...args: unknown[]) => {
+      console.debug('Umami mock track called with arguments:', JSON.stringify(args));
+      return undefined;
+    },
+    identify: async (...args: unknown[]) => {
+      console.debug('Umami mock identify called with arguments:', JSON.stringify(args));
+    },
   };
 }
 
